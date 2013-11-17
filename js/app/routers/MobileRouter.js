9ba134ced37e8a4f8a5f5942720014a861733811
DigitalVerve <info@digitalverve.de>
2013-11-17 23:35:09 +0100

    command line commit

--- js/app/routers/MobileRouter.js
+++ js/app/routers/MobileRouter.js
@@ -16,7 +16,7 @@ define(["jquery", "backbone", "models/Model", "views/View", "views/Aboutus", "vi
             // All of your Backbone Routes (add more)
             routes: {
                 // When there is no hash bang on the url, the home method is called
-                "": "home",
+                "": "testarea",
                 "home": "home",
 				"aboutus" : "aboutus",
 				"testarea" : "testarea"
@@ -24,25 +24,18 @@ define(["jquery", "backbone", "models/Model", "views/View", "views/Aboutus", "vi
 			
             home: function() {
                 new View();
-				$(document).ready(function(){
-					populateDeviceInfo();
-				});
+				populateDeviceInfo();
             },
             aboutus: function() {
                 new Aboutus();
-				$(document).ready(function(){
-					populateDeviceInfo();
-				});
+				populateDeviceInfo();
             },
             testarea: function() {
                 new Testarea();
-				$(document).ready(function(){
-					populateDeviceInfo();
-				});
+				populateDeviceInfo();
             },
             sidebar: function() {
                 new Sidebar();
-				report('sidebar');
             }
     
         });
