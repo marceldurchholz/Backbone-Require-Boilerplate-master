79db5a88a075f93b8993c5891b29c05424accbc0
DigitalVerve <info@digitalverve.de>
2013-11-19 11:26:44 +0100

    command line commit

--- js/app/init/MobileInit.js
+++ js/app/init/MobileInit.js
@@ -8,31 +8,15 @@ require(["jquery", "backbone", "routers/MobileRouter", "jquerymobile", "backbone
 
 	report('MobileInit.js','START');
     
-	// $('#body').append('<div id="sidebar"></div>');
 	
 	// Prevents all anchor click handling
     $.mobile.linkBindingEnabled = false;
-
     // Disabling this will prevent jQuery Mobile from handling hash changes
     $.mobile.hashListeningEnabled = false;
 	// $.mobile.hashListeningEnabled = true;
-
-	window.addEventListener('jqm-ready', function(){
-		alert("detected jqm-ready");
-		// run some code
-	});
-	document.addEventListener('mobileinit', function(){
-		// ^----- TYPO HERE (should be mobileinit)
-		alert('mobileinit');
-	});
 	
-	/*
-	$('body').on("click", ".showMenu", function (e) {
-		alert('bla');
-	});
-	*/
+	
 	$('body').on("click", "a.showMenu", function (e) {
-		// alert('clicked');
 		if (menuStatus != true) {
 			$(".ui-page-active").animate({
 				marginLeft: "175px",
@@ -50,7 +34,6 @@ require(["jquery", "backbone", "routers/MobileRouter", "jquerymobile", "backbone
 		}
 	});
 	$('body').on("click", "#menuelement a.contentLink", function (e) {
-		// alert('#menuelement a.contentLink clicked');
 		$(".ui-page-active").animate({
 			marginLeft: "0px",
 		}, 300, function () {
@@ -58,6 +41,125 @@ require(["jquery", "backbone", "routers/MobileRouter", "jquerymobile", "backbone
 		});
 	});
 	
+
+	var jqmReady = $.Deferred();
+	var pgReady = $.Deferred();
+
+	
+	var app = {
+		callback: function() {
+			report('var app','app.callback');
+			deviceReady = true;
+			cordovaIsLoaded = true;
+			report('app.callback','populateDeviceInfo()');
+			setTimeout("populateDeviceInfo();",1000);
+		},
+		initialize: function() {
+			report('MobileInit.js','var app.initialize();');
+			// this.callback = callback;
+			if(isMobile.any()) {
+				report("var app",">> IS NOT MOBILE");
+				this.bindEvents();
+			}
+			else {
+				report("var app",">> IS MOBILE");
+				//In case of web we ignore PG but resolve the Deferred Object to trigger initialization
+				pgReady.resolve();
+			}
+		},
+		bindEvents: function() {
+			// document.addEventListener('load', this.onDeviceReady, false);
+			// document.addEventListener('offline', this.onDeviceReady, false);
+			// document.addEventListener('online', this.onDeviceReady, false);
+			document.addEventListener('deviceready', this.onDeviceReady, false);
+			/*
+			if(!isMobile.any()) {
+				this.onDeviceReady();
+			}
+			*/
+		},
+		onDeviceReady: function() {
+			/*
+			report('MobileInit.js','onDeviceReady');
+			deviceReady = true;
+			cordovaIsLoaded = true;
+			app.receivedEvent('deviceready');
+			*/
+			app.receivedEvent('deviceready');
+	   },
+		receivedEvent: function(event) {
+			// report('***** var app','receivedEvent');
+			switch(event) {
+				case 'deviceready':
+					pgReady.resolve();
+					/*
+					setTimeout(function() {
+						report('MobileInit.js','app.receivedEvent w timeout 0');
+						$( document ).ready(function() {
+							report('MobileInit.js','app.receivedEvent AND document.ready in w timeout 0');
+						});
+					},0);
+					*/
+				break;
+			}
+		}
+	};
+	
+	
+	$(document).on("pageinit", function(event, ui) {
+	   report('document','pageinit');
+	   jqmReady.resolve();
+	});
+	/**
+	 * General initialization.
+	 */
+	$.when(jqmReady, pgReady).then(function() {
+	   //Initialization code here
+	   report("$.when","Frameworks ready.");
+	   if(app.callback) {
+		  app.callback();
+	   }
+	});
+
+	/*
+	Backbone.history.bind("route", function (route, router) {
+		report('MobileInit.js','Backbone.history.bind event >> '+router);
+		// alert(router);
+		$( document ).ready(function() {
+			// if ('router' == 'testarea') {
+				report('functions.js','populateDeviceInfo() for testarea when route changed and document ready');
+				setTimeout("populateDeviceInfo();",1000);
+			// }
+		});
+	});
+	*/
+	
+
+	app.initialize();
+    // Instantiates a new Mobile Router instance
+    new MobileRouter();
+
+  }
+
+);
+
+
+
+
+
+	//console.log(window.location.hash);
+	// opened file / location on- or offline?
+	// report('Backbone.history.location.href',Backbone.history.location.href);
+	/*
+	window.addEventListener('jqm-ready', function(){
+		alert("detected jqm-ready");
+		// run some code
+	});
+	document.addEventListener('mobileinit', function(){
+		// ^----- TYPO HERE (should be mobileinit)
+		alert('mobileinit');
+	});
+	*/
 	/*
 	$(document).on("pagebeforecreate", function(event, ui) {
 		report('pagebeforecreate');
@@ -91,89 +193,3 @@ require(["jquery", "backbone", "routers/MobileRouter", "jquerymobile", "backbone
 	// Backbone.history.bind("all", function (route, router) {
 	// $(document).ready(function() {
 	// if (!app) 
-	var app = {
-		// Application Constructor
-		initialize: function() {
-			this.bindEvents();
-			report('MobileInit.js','var app.initialize();');
-			// alert('var app');
-		},
-		// Bind Event Listeners
-		//
-		// Bind any events that are required on startup. Common events are:
-		// 'load', 'deviceready', 'offline', and 'online'.
-		bindEvents: function() {
-			document.addEventListener('deviceready', this.onDeviceReady, false);
-			if(!isMobile.any()) {
-				// setTimeout(this.onDeviceReady,1000);
-				this.onDeviceReady();
-			}
-		},
-		// deviceready Event Handler
-		//
-		// The scope of 'this' is the event. In order to call the 'receivedEvent'
-		// function, we must explicity call 'app.receivedEvent(...);'
-		onDeviceReady: function() {
-			report('MobileInit.js','onDeviceReady');
-			deviceReady = true;
-			cordovaIsLoaded = true;
-			this.receivedEvent();
-			// alert('onDeviceReady');
-			// $(document).on("pageshow", function(event, ui) {
-			// });
-			/*
-			document.addEventListener('DOMComponentsLoaded', function(){
-				alert('DOMComponentsLoaded are loaded!');
-				console.log("DOMComponentsLoaded are loaded!");
-			});
-			*/
-	   },
-		// Update DOM on a Received Event
-		receivedEvent: function() {
-			setTimeout(function() {
-				report('MobileInit.js','this.receivedEvent START');
-				$( document ).ready(function() {
-					// initApp();
-					/*
-					(function($) {
-						jqueryready = true;
-					}(jQuery));
-					*/
-					// alert('this.initialize >> receivedEvent >> Received Event');
-					// report('this.initialize >> receivedEvent','Received Event');
-				});
-			},0);
-		}
-	};
-	
-	
-	// opened file / location on- or offline?
-	// report('Backbone.history.location.href',Backbone.history.location.href);
-	
-	Backbone.history.bind("route", function (route, router) {
-		report('MobileInit.js','Backbone.history.bind event >> '+router);
-			// alert(router);
-			$( document ).ready(function() {
-				// if ('router' == 'testarea') {
-					report('functions.js','populateDeviceInfo() for testarea when route changed and document ready');
-					setTimeout(populateDeviceInfo,1000);
-				// }
-			});
-				
-		//console.log(window.location.hash);
-		// report('MobileInit.js','document.ready event');
-		// Handler for .ready() called.
-
-	// });
-		/*
-		report('MobileInit.js','document.ready END');
-		*/
-	});
-
-	app.initialize();
-    // Instantiates a new Mobile Router instance
-    new MobileRouter();
-
-  }
-
-);
