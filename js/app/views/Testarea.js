d792156cee8eb4203503d75cd649c2f4b3c3defb
DigitalVerve <info@digitalverve.de>
2013-11-22 22:22:42 +0100

    command line commit

--- js/app/views/Testarea.js
+++ js/app/views/Testarea.js
@@ -20,17 +20,19 @@ define(["jquery", "backbone", "models/Model", "text!templates/testarea.html", "t
 
             render: function() {
 
+				// sidebar
+				// $('#sidebar').html();
+				// var sidebarContent = _.template(bsidebar, {}); // this.template(dict);
+                this.sidebar = _.template(sidebar, {});
+				// Append the result to the view's element.
+				$('#sidebar').html(sidebar);
+				// Maintains chainability
+
+				
                 // Setting the view's template property using the Underscore template method
                 this.template = _.template(template, {});
                 // Dynamically updates the UI with the view's template
                 this.$el.html(this.template);
-
-				// sidebar
-				// $('#sidebar').html();
-				var sidebarContent = _.template(sidebar, {}); // this.template(dict);
-				// Append the result to the view's element.
-				$('#sidebar').html(sidebarContent);
-				// Maintains chainability
 
                 return this;
 
