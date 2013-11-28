d04bd23aeec68d6aa2f42e1bc1c6e29fc934fbf2
DigitalVerve <info@digitalverve.de>
2013-11-28 21:12:56 +0100

    command line commit

--- js/app/models/Model.js
+++ js/app/models/Model.js
@@ -1,30 +1,40 @@
 // Model.js
 // --------
-define(["jquery", "backbone"],
+define(["jquery", "backbone", "routers/MobileRouter", "jquerymobile", "backbone.validateAll"],
+// define(["backbone"],
 
-    function($, Backbone) {
+function($, Backbone, MobileRouter) {
+//   function(Backbone) {
 
-        // Creates a new Backbone Model class object
-        var Model = Backbone.Model.extend({
+		/*
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+			imagePath = fs.root.fullPath + "/logo.png"; // full file path
+			alert(imagePath);
+			// document.getElementById('camera_image_b').src = imagePath;
+		});
+		*/
+		
+		var Model = Backbone.Model.extend( {
+		
+			defaults: {
+				src: 'file:///D:/cordova/Backbone-Require-Boilerplate-master/public/Documents/profilepictures/default.jpg'
+			},
+			initialize: function(){
+				// alert("var Model initialized");
+				this.set({src: 'file:///D:/cordova/Backbone-Require-Boilerplate-master/public/Documents/profilepictures/mdurchholz.jpg'});
+			}
+		
+		} );
+		
+		// Model.title = 'foo';
+		// var amodel = new Model();
+		// amodel.set({title: 'Stewie Griffin'});
+		
+		
+		// var model = new Model();
 
-            // Model Constructor
-            initialize: function() {
-
-            },
-
-            // Default values for all of the Model attributes
-            defaults: {
-
-            },
-
-            // Gets called automatically by Backbone when the set and/or save methods are called (Add your own logic)
-            validate: function(attrs) {
-
-            }
-
-        });
-
-        // Returns the Model class
+		// alert(Model.title);
+		
         return Model;
 
     }
--- js/app/views/View.js
+++ js/app/views/View.js
@@ -1,8 +1,8 @@
 // View.js
 // -------
-define(["jquery", "backbone", "models/Model", "text!templates/view.html", "text!templates/sidebar.html"],
+define(["jquery", "backbone", "underscore", "models/Model", "text!templates/photoview.html", "text!templates/view.html", "text!templates/sidebar.html"],
 
-    function($, Backbone, Model, template, sidebar){
+    function($, Backbone, _, Model, photoTemplate, template, sidebar){
 
         var View = Backbone.View.extend({
 
@@ -10,6 +10,25 @@ define(["jquery", "backbone", "models/Model", "text!templates/view.html", "text!
 
             initialize: function() {
 
+				// _.bindAll(this, 'render');
+				
+				this.profilePictureModel = new Model;
+				// var keys = Object.keys(this.collection);
+				/*
+				for (var key in obj) {
+					if (obj.hasOwnProperty(key)) {
+						alert(key);
+						alert(obj.key);
+						// useful code here
+					}
+				}
+				*/
+				/*
+				this.collection = new Model;
+				_.bindAll(this, "render");
+				this.collection.bind( "reset", this.render );
+				*/
+				
                 this.render();
 
             },
@@ -18,23 +37,62 @@ define(["jquery", "backbone", "models/Model", "text!templates/view.html", "text!
 				
             },
 
-            render: function() {
-			
+            render: function( collection ) {
+
+				// alert('bla');
+				
+				// var compiled_template = _.template(photoTemplate);
+				//// var collection = this.collection;
+				// var $el = $(this.el);
+				
+				// var model = new Model;
+
+				// mobileSearch.utils.loadPrompt( "Loading photo..." );
+				// $( '#photo .ui-title' ).html( 'Photo view' );
+				/*
+				var mytext = '',
+				mytext = compiled_template( { results: collection.models } );
+				alert(mytext);
+				*/
+				// var collection = this.collection;
+				// alert(collection.title);
+				// $('#page-content').html('sss');
+
+				/*
+				setTimeout( function() {
+					$('photo').listview('refresh');
+				}, 0 );
+				*/
+				
+				// results: collection.models
+				// alert(this.collection);
+				var profilePicture = this.profilePictureModel.attributes;
+				// alert(profilePicture.src);
+				// alert(profilePicture.src);
+				
 				// sidebar
                 this.sidebar = _.template(sidebar, {});
 				// Append the result to the view's element.
-				$('#sidebar').html(sidebar);
+				// alert(sidebar);
+				$('.sidebar').html(sidebar);
 				// Maintains chainability
 				
                 // Setting the view's template property using the Underscore template method
-                this.template = _.template(template, {});
-                // Dynamically updates the UI with the view's template
+                // this.template = _.template(template, {});
+				// this.template = _.template(template, { profilePicture_src: profilePicture.src });
+				// Dynamically updates the UI with the view's template
                 // this.$el.html(this.template);
+				// this.template = _.template(template, {src: 'ououou'}, {variable: 'profilePicture'});
+				// this.template = _.template(template);
+                
+				template = _.template(template, {src: profilePicture.src}, {variable: 'profilePicture'});
+				
+				// alert(this.template);
 				$('#page-content').html(template);
 
 				// this.$el.trigger('create');
 				$('#body').trigger('create');
-
+				
                 return this;
 
             }
