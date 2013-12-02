ffd93016572b29e3badc0036598259f736371362
DigitalVerve <info@digitalverve.de>
2013-12-02 19:38:22 +0100

    command line commit

--- js/app/views/ProfileView.js
+++ js/app/views/ProfileView.js
@@ -20,13 +20,11 @@ define(["jquery", "backbone", "models/Profile", "models/System", "text!templates
 				this.model = '[{"id":1,"name":"Lisa"},{"id":2,"name":"Paul"}]';
 				*/
 				// _.each(this.model.models, function(profile){
-				/*
 				_.each(this.collection, function(profile){
 					// var profileTemplate = this.template(profile.toJSON());
 					// $(this.el).append(profileTemplate);
 					alert('each function');
 				}, this);
-				*/
 				/*
 				*/
 				// alert($('#profiles').html());
