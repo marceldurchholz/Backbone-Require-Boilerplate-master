2cd2b15da04a2c7a785c47fb0fab71e13375e49b
DigitalVerve <info@digitalverve.de>
2013-12-09 22:36:34 +0100

    command line commit

--- js/app/views/Videodetails.js
+++ js/app/views/Videodetails.js
@@ -1,8 +1,8 @@
 // Videos.js
 // -------
-define(["jquery", "backbone", "models/VideoModel", "collections/videosCollection", "views/VideoView", "text!templates/videosDetails.html", "views/videoDetailsView", "text!templates/sidebar.html"],
+define(["jquery", "backbone", "models/VideoModel", "collections/videosCollection", "views/VideoView", "text!templates/videosDetails.html", "text!templates/sidebar.html"],
 
-    function($, Backbone, VideoModel, videosCollection, Video, videos, videoDetailsView, sidebar){
+    function($, Backbone, VideoModel, videosCollection, Video, videos, sidebar){
 		
 			var Videos = Backbone.View.extend({
 			
@@ -46,8 +46,8 @@ define(["jquery", "backbone", "models/VideoModel", "collections/videosCollection
 					this.sidebar = _.template(sidebar, {});
 					$('#sidebar').html(sidebar);
 					
-					this._template = _.template(videos, {});
-					this.$el.html(this._template);
+					// this._template = _.template(videos, {});
+					// this.$el.html(this._template);
 					// console.log('this._videosCollection.models');
 					// console.log(this._videosCollection.models);
 					// this.nestedView = new videoDetailsView({collection: this._videosCollection.models}).render();
