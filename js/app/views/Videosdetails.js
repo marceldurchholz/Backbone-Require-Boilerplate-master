6351ddcb77f6129770c0e314fc7c7033571dc139
DigitalVerve <info@digitalverve.de>
2013-12-11 15:35:29 +0100

    command line commit

--- js/app/views/Videosdetails.js
+++ js/app/views/Videosdetails.js
@@ -8,39 +8,6 @@ define(["jquery", "backbone", "models/VideoModel", "collections/videosCollection
 			
 				el: "#page-content",
 				attributes: {"data-role": 'content'},
-				createVideo: function () {
-					if (this._videosCollection.online==0) {
-						// this._videosCollection._localStorage_users.create(new Video({"fullname": "offline James King", "device": "5645-6543-5415-5233", "credits": "120", "pictureurl": "http://www.redner24.de/typo3temp/GB/Durchholz_Marcel_4c_1090c3626b_Durc_a4ff6064ff.jpg"}));
-						alert('in offline mode you can not add data');
-					}
-					else {
-						var username = ''+Math.floor((Math.random()*10000)+1);
-						var password = ''+Math.floor((Math.random()*10000)+1);
-						// this._videosCollection._localStorage_users.create(new VideoModel({"fullname": "offline James King", "device": "5645-6543-5415-5233", "credits": "120", "pictureurl": "http://www.redner24.de/typo3temp/GB/Durchholz_Marcel_4c_1090c3626b_Durc_a4ff6064ff.jpg"}));
-						this.create(new VideoModel({"uploader": "042cb1572ffbea5d", "videourl": "http://xyz.de.com.uk", "title": "This is a video title", "description": "This is a description", "price": "35", "thumbnailurl": "http://www.cbr250r.com.au/images/video-thumbnail.jpg"}));
-					}
-					return(false);
-				},
-				create: function(model) {
-					_thisView = this;
-					$.ajax('http://dominik-lohmann.de:5000/videos', {
-					  type: "POST",
-					  contentType: "application/json",
-					  data: JSON.stringify(model.attributes),
-					  success: function(todo) {
-						_thisView.fetch();
-					  }, 
-					  error: function(xhr) {
-						console.log(xhr);
-						alert(xhr);
-					  }
-					});
-					return(false);
-				},
-				bindEvents: function() {
-					var _thisView = this;
-					this.$el.off('click','.createVideo').on('click','.createVideo',function(){_thisView.createVideo();});
-				},
 				initializeCollection:function(options) {
 					// alert(this.id);
 					// var myCollection = new Collection([ new Model({id: 'smith'}) ]);
@@ -66,9 +33,9 @@ define(["jquery", "backbone", "models/VideoModel", "collections/videosCollection
 					// console.log(models);
 					// console.log('options');
 					// console.log(options);
-					//// this.initializeCollection(options);
+					this.initializeCollection(options);
 					// this._videosCollection = new videosCollection();
-					//// this.fetch(options);
+					this.fetch(options);
 				},
 				render: function() {
 					// this.bindEvents();
