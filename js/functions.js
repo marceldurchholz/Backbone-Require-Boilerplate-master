13defecdcc61b45f4f45ab2f3bde4e48745a590d
DigitalVerve <info@digitalverve.de>
2014-01-03 18:38:39 +0100

    command line commit

--- js/functions.js
+++ js/functions.js
@@ -703,12 +703,13 @@ function postDeviceReadyActions(){
     modifyiOS7StatusBar
 / ----------------------------------------------------------- */
 function modifyiOS7StatusBar(){
+	if (window.device.version) alert('>> '+window.device.version);
     if (isMobile.any()) {
 		try{
 			// StatusBar.overlaysWebView(false);
 			// StatusBar.styleLightContent();
 			// StatusBar.backgroundColorByName("black");
-			StatusBar.hide();
+			StatusBar.show();
 			if (parseFloat(window.device.version) === 7.0) {
 				document.body.style.marginTop = "20px";
 			}
