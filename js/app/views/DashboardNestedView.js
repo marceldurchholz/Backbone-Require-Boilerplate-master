bea83c8682c781744bdb7807c07bb92f0da75b0c
DigitalVerve <info@digitalverve.de>
2014-02-28 18:58:27 +0100

    command line commit

--- js/app/views/DashboardNestedView.js
+++ js/app/views/DashboardNestedView.js
@@ -12,15 +12,30 @@ define(["jquery", "backbone", "text!templates/DashboardNestedViewPage.html"],
 				this.$el.hide();
 				showModal();
 				dpd.users.me(function(me) {
-					if (me) _thisViewDashboardNested.initializeme();
+					if (me) _thisViewDashboardNested.initializedata();
 					else system.redirectToUrl('#login');
 				});
 			},
-			initializeme: function() {
-				console.log('initializeme DashboardNestedView.js');
-				$(this.el).html('loading...');
+			initializedata: function() {
+				console.log('initializedata DashboardNestedView.js');
+				// $(this.el).html('loading...');
 				_thisViewDashboardNested.me = window.me;
-				_thisViewDashboardNested.render();
+				
+				var query = {  readby: {$nin: [me.id]},  $fields: {id: 1}, receiver:me.id, $sort:{cdate:1}};
+				// dpd.messages.get(query, function (allmessagesdata) {
+				// var query = "";
+				dpd.messages.get(query, function(messagesdata) {
+					if (messagesdata) {
+						console.log(messagesdata.length);
+						_thisViewDashboardNested.me.newmsgs = messagesdata.length;
+						// return(false);
+						_thisViewDashboardNested.render();
+					}
+					else {
+						alert('error ? ! ?');
+						// console.log('you are not logged in');
+					}
+				});
 			},
 			fetch: function() {	
 				_thisViewDashboardNested = this;
@@ -67,6 +82,7 @@ define(["jquery", "backbone", "text!templates/DashboardNestedViewPage.html"],
 					id: _thisViewDashboardNested.me.id
 					, pictureurl: _thisViewDashboardNested.me.pictureurl
 					, fullname: _thisViewDashboardNested.me.fullname
+					, newmsgs: _thisViewDashboardNested.me.newmsgs
 				},{variable: 'user'});
 				$(this.el).html(htmlContent);
 				hideModal();
