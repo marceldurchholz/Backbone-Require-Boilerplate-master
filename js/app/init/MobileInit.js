b9a574b2f8c1a6811766d38735de625067128c62
DigitalVerve <info@digitalverve.de>
2014-03-07 23:27:02 +0100

    command line commit

--- js/app/init/MobileInit.js
+++ js/app/init/MobileInit.js
@@ -6,41 +6,6 @@ require(["jquery", "backbone", "routers/MobileRouter", "jquerymobile", "backbone
 
   function($, Backbone, MobileRouter, LocalStorageAdapter) {
 
-	/*
-	dpd.messages.get({}, function (arr,err) {
-		if(err) {
-			return console.log(err);
-		}
-		$.each( arr, function( key, message ) {
-			dpd.messages.del(message.id, function (err) {
-			  if(err) console.log(err);
-			});						
-		});
-	});
-
-	dpd.orders.get({}, function (arr,err) {
-		if(err) {
-			return console.log(err);
-		}
-		$.each( arr, function( key, message ) {
-			dpd.orders.del(message.id, function (err) {
-			  if(err) console.log(err);
-			});						
-		});
-	});
-
-	dpd.users.get({}, function (arr,err) {
-		if(err) {
-			return console.log(err);
-		}
-		$.each( arr, function( key, message ) {
-			dpd.users.put(message.id, {"purchases":[], "followers":[], "interests":[], "following":[]}, function (result, err) {
-			  if(err) console.log(err);
-			});						
-		});
-	});
-	*/
-
 	// alert('mobileinit.js');
 	
 	/*
