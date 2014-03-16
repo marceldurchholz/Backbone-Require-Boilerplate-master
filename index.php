f31987d0b06e3a2b94d2b7ff65a42617ad1b10df
DigitalVerve <info@digitalverve.de>
2014-03-16 19:11:56 +0100

    command line commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7 ui-mobile-rendering" lang="en"> <![endif]-->
+	<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8 ui-mobile-rendering" lang="en"> <![endif]-->
+	<!--[if IE 8]>    <html class="no-js lt-ie9 ui-mobile-rendering" lang="en"> <![endif]-->
+	<!--[if gt IE 8]><!--> <html class="no-js ui-mobile-rendering" lang="en"> <!--<![endif]-->
+	<head>
+		<base href="">
+		<title>APPinaut</title>
+		<meta name="description" content="Der APPinaut ist eine Mobile Learning App. Im Multimediaformat kann den wissensdurstigen Nutzern des APPinaut Wissen angeboten werden." />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="format-detection" content="telephone=no" />
+		<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
+		<meta http-equiv="cache-control" content="no-cache"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<link rel="stylesheet" href="online/css/iframestyles.css" />
+	</head>
+	<body>
+		<div style="text-align: center;">
+			<div id="iphone4">
+				<iframe id="mockupiframe" scrolling="no" allowtransparency="true" frameborder="0" border="0" src="index.html"></iframe>
+			</div>
+		</div>
+	</body>
+</html>
