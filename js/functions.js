8fdd350176c6218ce04538e2eeb3902dbbc4ca7d
DigitalVerve <info@digitalverve.de>
2014-03-18 16:00:02 +0100

    command line commit

--- js/functions.js
+++ js/functions.js
@@ -591,7 +591,8 @@ try {
 
 	function isPhoneGap() {
 		// return (cordova || PhoneGap || phonegap) && /^file:\/{3}[^\/]/i.test(window.location.href) && /ios|iphone|ipod|ipad|android/i.test(navigator.userAgent);
-		return /^file:\/{3}[^\/]/i.test(window.location.href) && /ios|iphone|ipod|ipad|android/i.test(navigator.userAgent);
+		// return /^file:\/{3}[^\/]/i.test(window.location.href) && /ios|iphone|ipod|ipad|android/i.test(navigator.userAgent);
+		return /^file:\/{3}[^\/]/i.test(window.location.href) && /iphone|ipod|ipad/i.test(navigator.userAgent);
 	}
 
 	function checkFileExists(fileName){
